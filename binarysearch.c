ad428e3ab7f2c6d78ac9689acc0bbaf00f9e575f
11249a409-alt <11249a409@kanchiuniv.ac.in>
2026-04-17 23:32:55 +0530

    binarysearch.c
    Updated the comments and formatting in the binary search program for clarity. Removed unnecessary markdown syntax and improved the structure of the algorithm section.

--- binarysearch.c
+++ binarysearch.c
@@ -1,44 +1,36 @@
-## **AIM**
-
-To write a C program to analyze the time required to perform **binary search** on a sorted array of 10,000 randomly generated numbers using the `clock()` function.
-
----
-
-## **ALGORITHM**
-
-1. Start the program.
-2. Declare an array of size 10,000.
-3. Generate random numbers using `rand()`.
-4. Sort the array using **bubble sort**.
-5. Read the key element from the user.
-6. Start the timer using `clock()`.
-7. Perform binary search:
-
-   * Set `low = 0`, `high = SIZE - 1`
-   * Find `mid = (low + high) / 2`
-   * Compare key with middle element
-   * Adjust search range accordingly
-8. Stop the timer.
-9. Calculate time in **clock ticks**:
-
-   ```
-   time = end - start
-   ```
-10. Display index and time taken.
-11. Stop.
+AIM
+To write a C program to analyze the time required to perform binary search on a sorted array of 10000 randomly generated numbers using the clock() function.
 
 ---
 
-## **SOURCE CODE**
+ALGORITHM
 
-```c
+1. Start the program
+2. Declare an array of size 10000
+3. Generate random numbers using rand()
+4. Sort the array using bubble sort
+5. Read the key element from the user
+6. Start the timer using clock()
+7. Perform binary search
+   set low = 0 and high = SIZE - 1
+   find mid = (low + high) / 2
+   compare key with middle element
+   update low or high accordingly
+8. Stop the timer
+9. Calculate time using end - start
+10. Display index and time taken
+11. Stop
+
+---
+
+SOURCE CODE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define SIZE 10000
 
-// Bubble Sort Function
 void bubbleSort(int arr[])
 {
     int i, j, temp;
@@ -56,7 +48,6 @@ void bubbleSort(int arr[])
     }
 }
 
-// Binary Search Function
 int binarySearch(int arr[], int key)
 {
     int low = 0, high = SIZE - 1, mid;
@@ -83,32 +74,24 @@ int main()
     clock_t start, end;
     double time_taken;
 
-    // Generate random numbers
     for(i = 0; i < SIZE; i++)
     {
         arr[i] = rand() % 100000;
     }
 
-    // Sort the array
     bubbleSort(arr);
 
-    // Input key
     printf("Enter key: ");
     scanf("%d", &key);
 
-    // Start time
     start = clock();
 
-    // Binary search
     index = binarySearch(arr, key);
 
-    // End time
     end = clock();
 
-    // Time in clock ticks
     time_taken = (double)(end - start);
 
-    // Output
     if(index != -1)
         printf("key st %d with index at %d,time taken as is %.5f clock ticks\n", key, index);
     else
@@ -120,10 +103,11 @@ int main()
 
 ---
 
-## **OUTPUT**
+OUTPUT
 
-```
 Enter key: 523
 key st 523 with index at 4876,time taken as is 2.00000 clock ticks
-```
+
+---
+
 
